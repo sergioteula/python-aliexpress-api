65aaf864fc9e0672c92d312d4222d38043ad7166
Sergio Abad <agutrot93@gmail.com>
2024-12-20 22:24:00 +0100

    adjusted version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='python-aliexpress-api',
-    version='3.0.1',
+    version='3.1.0',
     author='Sergio Abad',
     author_email='sergio.abad@bytelix.com',
     description='AliExpress API wrapper for Python',
